042d66e6178a73f8ef9764d821858cce148239db
Michael Laß <bevan@bi-co.net>
2023-12-09 23:57:58 +0100

    Common Lisp solution for the very first task

---
+++ 01/1/solve.lisp
@@ -0,0 +1,22 @@
+(defun readin_file (filename)
+    (with-open-file (stream filename)
+        (loop for line = (read-line stream nil)
+            while line
+            collect line)))
+
+(defun firstNumber (chars)
+    (if (digit-char-p (first chars))
+        (digit-char-p (first chars))
+        (firstNumber (rest chars))))
+
+(defun solveOne (input)
+    (+ (* 10 (firstNumber (coerce input 'list)))
+       (firstNumber (reverse (coerce input 'list)))))
+
+(defun solveAll ()
+    (map 'list 'solveOne (readin_file "input")))
+
+(format t "~D~%"
+    (apply '+ (solveAll)))
+
+(quit)
