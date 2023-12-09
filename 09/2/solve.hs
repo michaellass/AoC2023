383d1da993f8de702fcb0bba07449fac1fb4743f
Michael Laß <bevan@bi-co.net>
2023-12-09 09:52:39 +0100

    Solution for day 9

---
+++ 09/2/solve.hs
@@ -0,0 +1,26 @@
+import Data.List.Split (splitOn)
+
+differences [x] = []
+differences (x : xs) = head xs - x : differences xs
+
+buildSequence i [d] = i - d : [i]
+buildSequence i (d : ds) = i - d : buildSequence (i + head ds) ds
+
+allDifferences l
+  | all (== 0) nextDiff = [nextDiff]
+  | otherwise = nextDiff : allDifferences nextDiff
+  where
+    nextDiff = differences l
+
+rebuild initials diffs
+  | length initials == 1 = buildSequence (head initials) $ replicate (length (head diffs) + 1) 0
+  | otherwise = buildSequence (head initials) $ rebuild (tail initials) (tail diffs)
+
+processLine l = rebuild initials diffs
+  where
+    diffs = allDifferences l
+    initials = head l : map head (take (length diffs - 1) diffs)
+
+solve x = sum $ map (head . processLine . map read) x
+
+main = readFile "input" >>= print . solve . map (splitOn " ") . lines
