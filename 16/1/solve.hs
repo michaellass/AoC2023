018002b9d640137af1c452d5aacb1faaf3010a40
Michael Laß <bevan@bi-co.net>
2023-12-16 16:04:48 +0100

    Clean up day 16

--- 16/1/solve.hs
+++ 16/1/solve.hs
@@ -8,7 +8,7 @@ getAt field row col
   | col >= length (head field) = Coord {row = row, col = col, val = '.', ltr = True, rtl = True, ttb = True, btt = True}
   | otherwise = (field !! row) !! col
 
-data Direction = LeftD | RightD | UpD | DownD
+data Direction = LeftD | RightD | UpD | DownD deriving (Eq, Show)
 
 out '|' DownD = [DownD]
 out '|' UpD = [UpD]
@@ -28,19 +28,6 @@ out '\\' LeftD = [UpD]
 out '\\' RightD = [DownD]
 out '.' inD = [inD]
 
-combineCoords
-  Coord {row = r1, col = c1, val = v1, ltr = ltr1, rtl = rtl1, ttb = ttb1, btt = btt1}
-  Coord {row = r2, col = c2, val = v2, ltr = ltr2, rtl = rtl2, ttb = ttb2, btt = btt2} =
-    Coord
-      { row = r1,
-        col = c1,
-        val = v1,
-        ltr = ltr1 || ltr2,
-        rtl = rtl1 || rtl2,
-        ttb = ttb1 || ttb2,
-        btt = btt1 || btt2
-      }
-
 updateCoordInField
   f
   Coord {row = r, col = c, val = v, ltr = ltr, rtl = rtl, ttb = ttb, btt = btt} =
@@ -54,30 +41,23 @@ updateCoordInField
     where
       effectedRow = f !! r
 
-updateCoordsInField = foldl updateCoordInField
-
 fieldToStrings = map (map (\c -> if ltr c || rtl c || ttb c || btt c then '#' else '.'))
 
-updatedNext f c UpD = if next /= updatedNext then Just (updatedNext, UpD) else Nothing
-  where
-    next = getAt f (row c - 1) (col c)
-    updatedNext = Coord {row = row next, col = col next, val = val next, ltr = ltr next, rtl = rtl next, ttb = ttb next, btt = True}
-updatedNext f c DownD = if next /= updatedNext then Just (updatedNext, DownD) else Nothing
-  where
-    next = getAt f (row c + 1) (col c)
-    updatedNext = Coord {row = row next, col = col next, val = val next, ltr = ltr next, rtl = rtl next, ttb = True, btt = btt next}
-updatedNext f c LeftD = if next /= updatedNext then Just (updatedNext, LeftD) else Nothing
-  where
-    next = getAt f (row c) (col c - 1)
-    updatedNext = Coord {row = row next, col = col next, val = val next, ltr = ltr next, rtl = True, ttb = ttb next, btt = btt next}
-updatedNext f c RightD = if next /= updatedNext then Just (updatedNext, RightD) else Nothing
-  where
-    next = getAt f (row c) (col c + 1)
-    updatedNext = Coord {row = row next, col = col next, val = val next, ltr = True, rtl = rtl next, ttb = ttb next, btt = btt next}
+updateCoord c UpD = Coord {row = row c, col = col c, val = val c, ltr = ltr c, rtl = rtl c, ttb = ttb c, btt = True}
+updateCoord c DownD = Coord {row = row c, col = col c, val = val c, ltr = ltr c, rtl = rtl c, ttb = True, btt = btt c}
+updateCoord c LeftD = Coord {row = row c, col = col c, val = val c, ltr = ltr c, rtl = True, ttb = ttb c, btt = btt c}
+updateCoord c RightD = Coord {row = row c, col = col c, val = val c, ltr = True, rtl = rtl c, ttb = ttb c, btt = btt c}
 
-step f (c, d) = mapMaybe (updatedNext f c) outDir
+updatedNext f c dir = if next /= updatedNext then Just (updatedNext, dir) else Nothing
   where
-    outDir = out (val c) d
+    next = case dir of
+      UpD -> getAt f (row c - 1) (col c)
+      DownD -> getAt f (row c + 1) (col c)
+      LeftD -> getAt f (row c) (col c - 1)
+      RightD -> getAt f (row c) (col c + 1)
+    updatedNext = updateCoord next dir
+
+step f (c, d) = mapMaybe (updatedNext f c) $ out (val c) d
 
 process field [] = field
 process field toEvaluate
@@ -85,20 +65,18 @@ process field toEvaluate
   | otherwise = process updatedNext nextDirs
   where
     nextDirs = concatMap (step field) toEvaluate
-    updatedNext = updateCoordsInField field (map fst nextDirs)
-
-markFirst f = updateCoordInField f updatedC
-  where
-    c = head . head $ f
-    updatedC = Coord {row = row c, col = col c, val = val c, ltr = True, rtl = rtl c, ttb = ttb c, btt = btt c}
+    updatedNext = foldl updateCoordInField field $ map fst nextDirs
 
 createCoord row = map (\(col, val) -> Coord {row = row, col = col, val = val, ltr = False, rtl = False, ttb = False, btt = False})
 
 augmentCoordinates = zipWith createCoord [0 ..] . map (zip [0 ..])
 
-solve x = length . filter (== '#') . concat . fieldToStrings $ process field initialDirs
+markVisited f (c, d) = updateCoordInField f $ updateCoord c d
+
+solve x = length . filter (== '#') . concat . fieldToStrings $ process initialField [initialDir]
   where
-    field = markFirst . augmentCoordinates $ lines x
-    initialDirs = [(head . head $ field, RightD)]
+    field = augmentCoordinates $ lines x
+    initialDir = (head . head $ field, RightD)
+    initialField = markVisited field initialDir
 
 main = readFile "input" >>= print . solve
--- 16/2/solve.hs
+++ 16/2/solve.hs
@@ -28,19 +28,6 @@ out '\\' LeftD = [UpD]
 out '\\' RightD = [DownD]
 out '.' inD = [inD]
 
-combineCoords
-  Coord {row = r1, col = c1, val = v1, ltr = ltr1, rtl = rtl1, ttb = ttb1, btt = btt1}
-  Coord {row = r2, col = c2, val = v2, ltr = ltr2, rtl = rtl2, ttb = ttb2, btt = btt2} =
-    Coord
-      { row = r1,
-        col = c1,
-        val = v1,
-        ltr = ltr1 || ltr2,
-        rtl = rtl1 || rtl2,
-        ttb = ttb1 || ttb2,
-        btt = btt1 || btt2
-      }
-
 updateCoordInField
   f
   Coord {row = r, col = c, val = v, ltr = ltr, rtl = rtl, ttb = ttb, btt = btt} =
@@ -54,30 +41,23 @@ updateCoordInField
     where
       effectedRow = f !! r
 
-updateCoordsInField = foldl updateCoordInField
-
 fieldToStrings = map (map (\c -> if ltr c || rtl c || ttb c || btt c then '#' else '.'))
 
-updatedNext f c UpD = if next /= updatedNext then Just (updatedNext, UpD) else Nothing
-  where
-    next = getAt f (row c - 1) (col c)
-    updatedNext = Coord {row = row next, col = col next, val = val next, ltr = ltr next, rtl = rtl next, ttb = ttb next, btt = True}
-updatedNext f c DownD = if next /= updatedNext then Just (updatedNext, DownD) else Nothing
-  where
-    next = getAt f (row c + 1) (col c)
-    updatedNext = Coord {row = row next, col = col next, val = val next, ltr = ltr next, rtl = rtl next, ttb = True, btt = btt next}
-updatedNext f c LeftD = if next /= updatedNext then Just (updatedNext, LeftD) else Nothing
-  where
-    next = getAt f (row c) (col c - 1)
-    updatedNext = Coord {row = row next, col = col next, val = val next, ltr = ltr next, rtl = True, ttb = ttb next, btt = btt next}
-updatedNext f c RightD = if next /= updatedNext then Just (updatedNext, RightD) else Nothing
-  where
-    next = getAt f (row c) (col c + 1)
-    updatedNext = Coord {row = row next, col = col next, val = val next, ltr = True, rtl = rtl next, ttb = ttb next, btt = btt next}
+updateCoord c UpD = Coord {row = row c, col = col c, val = val c, ltr = ltr c, rtl = rtl c, ttb = ttb c, btt = True}
+updateCoord c DownD = Coord {row = row c, col = col c, val = val c, ltr = ltr c, rtl = rtl c, ttb = True, btt = btt c}
+updateCoord c LeftD = Coord {row = row c, col = col c, val = val c, ltr = ltr c, rtl = True, ttb = ttb c, btt = btt c}
+updateCoord c RightD = Coord {row = row c, col = col c, val = val c, ltr = True, rtl = rtl c, ttb = ttb c, btt = btt c}
 
-step f (c, d) = mapMaybe (updatedNext f c) outDir
+updatedNext f c dir = if next /= updatedNext then Just (updatedNext, dir) else Nothing
   where
-    outDir = out (val c) d
+    next = case dir of
+      UpD -> getAt f (row c - 1) (col c)
+      DownD -> getAt f (row c + 1) (col c)
+      LeftD -> getAt f (row c) (col c - 1)
+      RightD -> getAt f (row c) (col c + 1)
+    updatedNext = updateCoord next dir
+
+step f (c, d) = mapMaybe (updatedNext f c) $ out (val c) d
 
 process field [] = field
 process field toEvaluate
@@ -85,19 +65,13 @@ process field toEvaluate
   | otherwise = process updatedNext nextDirs
   where
     nextDirs = concatMap (step field) toEvaluate
-    updatedNext = updateCoordsInField field (map fst nextDirs)
-
-markFirst f (c, d) = updateCoordInField f updatedC
-  where
-    updatedC = case d of
-      RightD -> Coord {row = row c, col = col c, val = val c, ltr = True, rtl = rtl c, ttb = ttb c, btt = btt c}
-      LeftD -> Coord {row = row c, col = col c, val = val c, ltr = ltr c, rtl = True, ttb = ttb c, btt = btt c}
-      UpD -> Coord {row = row c, col = col c, val = val c, ltr = ltr c, rtl = rtl c, ttb = ttb c, btt = True}
-      DownD -> Coord {row = row c, col = col c, val = val c, ltr = ltr c, rtl = rtl c, ttb = True, btt = btt c}
+    updatedNext = foldl updateCoordInField field $ map fst nextDirs
 
 createCoord row = map (\(col, val) -> Coord {row = row, col = col, val = val, ltr = False, rtl = False, ttb = False, btt = False})
 
 augmentCoordinates = zipWith createCoord [0 ..] . map (zip [0 ..])
+
+markVisited f (c, d) = updateCoordInField f $ updateCoord c d
 
 solve x = maximum $ map (length . filter (== '#') . concat . fieldToStrings . uncurry process) initialInputs
   where
@@ -107,7 +81,7 @@ solve x = maximum $ map (length . filter (== '#') . concat . fieldToStrings . un
         ++ zip (last field) (repeat UpD)
         ++ zip (map head field) (repeat RightD)
         ++ zip (map last field) (repeat LeftD)
-    initialFields = map (markFirst field) initialDirs
+    initialFields = map (markVisited field) initialDirs
     initialInputs = zip initialFields [[x] | x <- initialDirs]
 
 main = readFile "input" >>= print . solve
